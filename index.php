3a66d9dd7ca41b360ea1fadccc816b352cf526c5
PedroHenriqueGomesSilva <133805605+PedroHenriqueGomesSilva@users.noreply.github.com>
2023-09-14 14:22:25 -0300

    Primeira Versão da Tela de Login

--- index.php
+++ index.php
@@ -1,2 +1,47 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <title>Login</title>
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <form action="" method="POST">
+            <div class="mb-3">
+                <label for="emailUsuario" class="form-label">Email:</label>
+                <input type="text" class="form-control" id="emailUsuario" name="email">
+            </div>
+            <div class="mb-3">
+                <label for="senhaUsuario" class="form-label">Senha:</label>
+                <input type="text" class="form-control" id="senhaUsuario" name="senha">
+                <button type="submit" class="btn btn-primary" name="logar">Login</button>
+            </div>
+        </form>
+    </div>
+
+</body>
+
+</html>
+
+<?php
+if (isset($_POST["logar"])) {
+    $usuario = new Usuario;
+    $usuario->nome = $_POST["usuario"];
+    $usuario->senha = $_POST["senha"];
+
+    $login = $usuario->logarUsuario($usuario->nome, $usuario->senha);
+
+    if ($login) {
+        header("Location:menu.php");
+    }else{
+        echo '<script>alert("Usuario ou Senha Inválidos!")</script>';
+    }
+}
+?>
