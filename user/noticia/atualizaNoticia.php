2c792cda83d9748415ad17f8dcbd787a6971b80f
Victória Cabral Quintério <quinterio.victoria@hotmail.com>
2023-11-16 03:16:22 -0300

    Mudei a label

--- user/noticia/atualizaNoticia.php
+++ user/noticia/atualizaNoticia.php
@@ -87,7 +87,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id']) && is_numeric($_GE
                             <textarea id="conteudo" name="conteudo" rows="15" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required><?php echo $noticia_atual[0]['ds_conteudo']; ?></textarea>
                         </div>
                         <div class="mb-6">
-                            <label for="urlImagem" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Data</label>
+                            <label for="urlImagem" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">URL da imagem</label>
                             <input type="text" id="urlImagem" name="urlImagem" value="<?php echo $noticia_atual[0]['im_capa_url']; ?>" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
                         </div>
                         <div class="mb-6">
